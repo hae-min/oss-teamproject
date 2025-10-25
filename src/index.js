d298e99b0343f86fba2b7b057a38d4c4b4533b23
unknown <mmdaggu2@gamil.com>
2025-10-25 17:09:54 +0900

    origin 수정

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "../Home";
-import Upload from "./pages/Upload";
-import Edit from "./pages/Edit";
-import Sort from "./pages/Sort";
-import NotFound from "./pages/NotFound";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/upload" element={<Upload />} />
-        <Route path="/edit/:id" element={<Edit />} />
-        <Route path="/sort" element={<Sort />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
