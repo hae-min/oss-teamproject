c0d9f564c0ce6241e4529b7e312ed9e844e43b41
unknown <mmdaggu2@gamil.com>
2025-10-25 19:25:01 +0900

    origin 수정

--- src/Profile.js
+++ src/Profile.js
@@ -1,7 +1,19 @@
 function MyProfile() {
   return (
-    <div>
-      여기에 HTML처럼 작성!
+    <div className="profile-container">
+      <h1 className="title">댕모도로</h1>
+      <div>내 정보</div>
+      <form>
+        <div>
+          <input type="text" name="nickname" placeholder="사용자 nickname입력"></input>
+          <button type="submit">검색</button>
+        </div>
+      </form>
+      <div className="user-info">
+        <div className="user-nickname">000</div>
+        <div className="user-studytime">총 공부 시간: 20시간</div>
+        <div className="user-img"></div>
+      </div>
     </div>
   );
 }
